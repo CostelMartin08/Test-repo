ad19ed45914f151db810f530ef63351a9147d51a
Costel <costelmartinescu2000@gmail.com>
2024-07-19 11:48:10 +0300

    first version

---
+++ js/elements.js
@@ -0,0 +1,160 @@
+
+export function createTitleAndPercent(product) {
+
+    const container = document.createElement('div');
+
+    const percent = document.createElement('p');
+    percent.className = 'percent';
+    percent.textContent = '-7%';
+
+    const title = document.createElement('h3');
+    title.textContent = product.name;
+    title.id = 'title';
+
+    container.appendChild(percent);
+    container.appendChild(title);
+
+    return container;
+
+}
+
+export function createAvailability() {
+
+    const availability = document.createElement('div');
+    availability.className = 'availability';
+
+    const code = document.createElement('p');
+    code.innerHTML = '<span>Κωδικός:</span> 04.1048';
+
+    const verticalLine = document.createElement('div');
+    verticalLine.className = 'vertical-line';
+
+    const delivery = document.createElement('p');
+
+    delivery.innerHTML = '<span>Διαθεσιμότητα:</span> Παράδοση σε 1 - 3 ημέρες';
+
+    availability.appendChild(code);
+    availability.appendChild(verticalLine);
+    availability.appendChild(delivery);
+
+    return availability;
+}
+
+export function createPrice(product) {
+    const price = document.createElement('div');
+    price.className = 'price';
+
+    const originalPrice = document.createElement('span');
+    originalPrice.className = 'original-price';
+    originalPrice.textContent = `${product.original_price} €`;
+
+    const discountPrice = document.createElement('span');
+    discountPrice.className = 'discount-price';
+    discountPrice.textContent = `${product.discounted_price} €`;
+
+    price.appendChild(originalPrice);
+    price.appendChild(discountPrice);
+
+    return price;
+}
+
+export function createAbout() {
+    const about = document.createElement('div');
+    about.className = 'about font-light';
+
+    const descriptionTitle = document.createElement('h5');
+    descriptionTitle.textContent = 'Περιγραφή:';
+
+    const description = document.createElement('p');
+    description.textContent = 'Έτοιμο Ready to go Spritz με την υπογραφή της Luxardo, ελαφρύ & λεμονάτο, όπως το limoncello!';
+
+    const quantity = document.createElement('p');
+    quantity.textContent = 'Ποσότητα';
+
+    about.appendChild(descriptionTitle);
+    about.appendChild(description);
+    about.appendChild(quantity);
+
+    return about;
+}
+
+
+export function createCheckout() {
+    const checkout = document.createElement('section');
+    checkout.className = 'checkout';
+
+    const count = document.createElement('div');
+    count.className = 'count';
+
+    const decrement = document.createElement('span');
+    decrement.textContent = '-';
+    decrement.addEventListener('click', () => updateQuantity(-1));
+
+    const quantity = document.createElement('p');
+    quantity.textContent = '1';
+    quantity.id = 'quantity';
+
+    const increment = document.createElement('span');
+    increment.textContent = '+';
+    increment.addEventListener('click', () => updateQuantity(1));
+
+    count.appendChild(decrement);
+    count.appendChild(quantity);
+    count.appendChild(increment);
+
+    const button = document.createElement('button');
+    button.className = 'button';
+    button.innerHTML = '<i class="fa-solid fa-cart-shopping"></i> Προσθήκη στο καλάθι';
+
+    checkout.appendChild(count);
+    checkout.appendChild(button);
+
+    return checkout;
+}
+
+
+export function createDetails() {
+    const details = document.createElement('section');
+    details.className = 'details';
+
+    const items = [
+        { label: 'Κατηγορία Ποτού:', value: 'RTDs, Έτοιμο cocktail' },
+        { label: 'Χώρα Προέλευσης:', value: 'Ιταλία' },
+        { label: 'Μέγεθος Φιάλης:', value: '200ml' },
+        { label: 'Αλκοόλ:', value: '3%' }
+    ];
+
+    items.forEach(item => {
+        const detailItem = document.createElement('div');
+        detailItem.className = 'details-item';
+
+        const label = document.createElement('p');
+        label.textContent = item.label;
+
+        const value = document.createElement('p');
+        value.textContent = item.value;
+
+        detailItem.appendChild(label);
+        detailItem.appendChild(value);
+        details.appendChild(detailItem);
+    });
+
+    return details;
+}
+
+export function createSpan(text, className) {
+    const span = document.createElement('span');
+    span.className = className;
+    span.textContent = text;
+    return span;
+}
+
+
+export function updateQuantity(change) {
+    const quantityElement = document.querySelector('#quantity');
+
+    let quantity = parseInt(quantityElement.textContent, 10);
+    
+    quantity = Math.max(1, quantity + change);
+    quantityElement.textContent = quantity;
+}
--- main.js
+++ main.js
@@ -1,58 +1,148 @@
 import 'swiper/css';
 import 'swiper/css/bundle';
 import Swiper from 'swiper/bundle';
-import data from './mock-data.json';
+
+import {
+  createTitleAndPercent,
+  createAvailability,
+  createPrice,
+  createAbout,
+  createCheckout,
+  createDetails,
+  createSpan,
+} from './js/elements';
+
 
 import './style.css'
 
+function Box(product) {
 
-document.querySelector('#app').innerHTML = `
-  
-  <div class="swiper">
+  const container = document.createElement('div');
+  container.id = 'app';
+  container.className = 'container';
 
-    <div class="swiper-wrapper">
-     
-      <div class="swiper-slide">${data[0].name}</div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
-      <div class="swiper-slide">Slide 4</div>
-      <div class="swiper-slide">Slide 5</div>
-      <div class="swiper-slide">Slide 6</div>
+  function createSectionFirst(product) {
+    const section = document.createElement('section');
+    section.className = 'child first';
 
-    </div>
+    const img = document.createElement('img');
+    img.className = 'img';
+    img.src = product.image;
+    img.alt = product.name;
 
-    <div class="swiper-pagination"></div>
-
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-
-    <div class="swiper-scrollbar"></div>
-
-  </div>
-
-`
-
-const swiper = new Swiper('.swiper', {
-
-  direction: 'horizontal',
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    section.appendChild(img);
+    return section;
   }
 
+  function createSectionSecond(product) {
+    const section = document.createElement('section');
+    section.className = 'child second';
+
+    const titleandPercent = createTitleAndPercent(product);
+    const availability = createAvailability();
+    const price = createPrice(product);
+    const about = createAbout();
+    const checkout = createCheckout();
+    const details = createDetails();
+
+    section.appendChild(titleandPercent);
+    section.appendChild(availability);
+    section.appendChild(price);
+    section.appendChild(about);
+    section.appendChild(checkout);
+    section.appendChild(createSpan('*Στην τιμή συμπεριλαμβάνεται ο ΦΠΑ 24%', 'font-light'));
+    section.appendChild(document.createElement('hr'));
+    section.appendChild(details);
+
+    return section;
+  }
+
+  container.appendChild(createSectionFirst(product));
+  container.appendChild(createSectionSecond(product));
+
+  return container;
+}
+
+
+function createSwiper(containerSelector, products) {
+
+  const swiperContainer = document.createElement('div');
+  swiperContainer.className = 'swiper';
+
+
+  const swiperWrapper = document.createElement('div');
+  swiperWrapper.className = 'swiper-wrapper';
+
+
+  products.forEach(product => {
+    const swiperSlide = document.createElement('div');
+    swiperSlide.className = 'swiper-slide';
+
+
+    const containerBox = Box(product);
+
+    swiperSlide.appendChild(containerBox);
+
+
+    swiperWrapper.appendChild(swiperSlide);
+  });
+
+
+  swiperContainer.appendChild(swiperWrapper);
+
+
+  const pagination = document.createElement('div');
+  pagination.className = 'swiper-pagination';
+  swiperContainer.appendChild(pagination);
+
+  const buttonPrev = document.createElement('div');
+  buttonPrev.className = 'swiper-button-prev';
+  swiperContainer.appendChild(buttonPrev);
+
+  const buttonNext = document.createElement('div');
+  buttonNext.className = 'swiper-button-next';
+  swiperContainer.appendChild(buttonNext);
+
+  const scrollbar = document.createElement('div');
+  scrollbar.className = 'swiper-scrollbar';
+  swiperContainer.appendChild(scrollbar);
+
+
+  const container = document.querySelector(containerSelector);
+  container.innerHTML = '';
+  container.appendChild(swiperContainer);
+
+
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    }
+  });
+
+  return swiper;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dataUrl = './mock-data.json';
+
+  fetch(dataUrl)
+    .then(response => response.json())
+    .then(products => {
+
+      createSwiper('#app', products);
+    })
+    .catch(error => {
+      console.error('Error fetching JSON:', error);
+    });
 });
-
-
-
 
 
